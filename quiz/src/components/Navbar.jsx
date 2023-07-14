716194cc99c212af75c45a3f4a6e5d8ca0d5ed0d
Gangaram1998 <ramggawade1998@gmail.com>
2023-07-14 16:38:25 +0530

    add

--- quiz/src/components/Navbar.jsx
+++ quiz/src/components/Navbar.jsx
@@ -5,10 +5,12 @@ import {
     Text
 } from "@chakra-ui/react"
 import logo from "../constant/quiz.png"
+import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
+    const navigate=useNavigate()
     return (
-        <Box  padding="10px 50px" display='flex' gap='10px'  alignItems="center" boxShadow= 'rgba(0, 0, 0, 0.24) 0px 3px 8px'>
+        <Box onClick={()=>navigate("/")}  padding="10px 50px" display='flex' gap='10px'  alignItems="center" boxShadow= 'rgba(0, 0, 0, 0.24) 0px 3px 8px'>
             <Image height={"100%"} width={"80px"} src={logo} alt="logo"/>
         </Box>
     )
