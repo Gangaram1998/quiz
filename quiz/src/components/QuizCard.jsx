397f3438a9c1459cd288314ebfbaf97286737352
Gangaram1998 <ramggawade1998@gmail.com>
2023-07-14 12:33:07 +0530

    add

--- quiz/src/components/QuizCard.jsx
+++ quiz/src/components/QuizCard.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 export const QuizCard = ({i,id,desc,level}) => {
   const navigate=useNavigate()
   return (
-    <Box padding="20px 10px"  boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px" maxW={"300px"} w="300px"  h='250px'   display="flex" flexDirection="column" gap="20px" >
+    <Box padding="20px 10px"  boxShadow="rgba(0, 0, 0, 0.24) 0px 3px 8px" maxW={"300px"} w="300px"  h='300px'   display="flex" flexDirection="column" gap="20px" >
       <Text fontSize="24px" color="blue" fontWeight="700">Quiz : {i+1}</Text>
         <Text fontSize="20px" fontWeight="700" color="gray.600">{desc}</Text>
         <Text fontSize="18px" fontWeight="600">level:{level}</Text>
